218dfda407df19e3b7b4fa51d4b79db894799b9d
giuse <giuseppedefilippo03@gmail.com>
2024-11-18 15:28:08 +0100

    tolto i commenti

--- script.js
+++ script.js
@@ -8,17 +8,14 @@ console.clear();
 function ciclo() {
     const url = `https://flynn.boolean.careers/exercises/api/random/mail`;
     let lista = document.getElementById("lista");
-    // const vuoto = [];
     for (let i = 0; i < 10; i++) {
         axios.get(url).then(risposta => {
-            //vuoto.push(risposta.data.response);
             lista.innerHTML += `<li>${risposta.data.response}</li>`;
 
 
         });
     };
-    // lista.innerHTML = vuoto.map(email => `<li>${email}</li>`).join('');
-    // console.log(vuoto);
+
 
 
 
