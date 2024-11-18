06d603cbf94f1bc8346d876eeeee9f80483a57dd
giuse <giuseppedefilippo03@gmail.com>
2024-11-18 15:53:42 +0100

    bonus

--- es2/script.js
+++ es2/script.js
@@ -3,24 +3,19 @@
 // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 
 
-console.clear();
 
-function ciclo() {
+console.clear();
+function generaEmail() {
     const url = `https://flynn.boolean.careers/exercises/api/random/mail`;
     let lista = document.getElementById("lista");
-    for (let i = 0; i < 10; i++) {
+    lista.innerHTML = "";
+    for (i = 0; i < 19; i++) {
         axios.get(url).then(risposta => {
             lista.innerHTML += `<li>${risposta.data.response}</li>`;
-
-
-        });
-    };
-
-
-
-
-
-
-
+        })
+    }
 }
-ciclo();
+document.getElementById("bottone").addEventListener("click", generaEmail);
+generaEmail();
+
+
